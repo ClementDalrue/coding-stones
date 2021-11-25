27fb0b145c1f7b609367ac5076d6a103e164ee37
adeleguillem <adele.guillem@gmail.com>
2021-11-25 22:00:14 +0100

    page propositions pas finie et routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Settings from "./settings";
 import Details from "./details";
+import Propositions from "./Propositions";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
-        test
+       
         <div className="App">
           <Routes>
             <Route exact path="/" element={<Settings />} />
+            <Route exact path="/propositions" element={<Propositions />} />
           <Route path="/details" element={<Details />} />
           </Routes>
         </div>
