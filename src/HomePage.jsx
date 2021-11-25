4d23482cf0065a239d1cef2a8fbc670a7e0f16e0
Mathieu Cachen <mathieu.cachen@gmail.com>
2021-11-25 22:15:37 +0100

    wip

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -8,31 +8,35 @@ const HomePage = () => {
                 <form className="homepage-form">
                 <div className="homepage-form-container">
                 <div className="homepage-form-toggle">
-                  <label>Blues</label>
+                  <label htmlFor="Blues">Blues</label>
                 <input className="homepage-form-input"
                 type="checkbox"
                 name="Blues"
+                id="Blues"
                 />
                 </div>
                 <div className="homepage-form-toggle" >
-                <label>Jazz</label>
+                <label htmlFor="Jazz">Jazz</label>
                 <input className="homepage-form-input"
                 type="checkbox"
                 name="Jazz"
+                id="Jazz"
                 />
                 </div>
                 <div className="homepage-form-toggle">
-                <label>Rap</label>
+                <label htmlFor="Rap">Rap</label>
                 <input className="homepage-form-input"
                 type="checkbox"
                 name="Rap"
+                id="Rap"
                 />
                 </div>
                 <div className="homepage-form-toggle">
-                <label>Reggae</label>
+                <label htmlFor="Rock">Reggae</label>
                 <input className="homepage-form-input"
                 type="checkbox"
                 name="Reggae"
+                id="Reggae"
                 />
                 </div>
                 <div className="homepage-form-toggle">
@@ -41,14 +45,16 @@ const HomePage = () => {
                 id="Rock"
                 type="checkbox"
                 name="Rock"
+                id="Rock"
                 />
                 </div>
                 </div>
-                <div className="homepage-label">
-                <label>Ton budget</label>
-                <input type="text" id="budget"></input>
-                </div>
-                <button className="homepage-form-button" type="submit">J'y vais</button>
+                <input className="homepage-form-input-budget"
+                type="text" 
+                id="budget"
+                placeholder="Votre budget"
+                />
+                <button className="homepage-form-button" type="submit">GO</button>
               </form>
             </div>
           );
