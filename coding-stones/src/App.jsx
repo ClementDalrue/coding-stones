a6aefcba1471de4582c4fc109fc7840725206569
adeleguillem <adele.guillem@gmail.com>
2021-11-25 18:04:27 +0100

    page 3 details

--- coding-stones/src/App.jsx
+++ coding-stones/src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import Settings from './settings'
+import Details from './details'
+
 
 function App() {
 
   return (
     <div className="App">
       <Settings />
+      <Details />
     </div>
   )
 }
