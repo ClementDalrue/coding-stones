a8426702c2b0412dd9db395a04eda17d2c726399
Mathieu Cachen <mathieu.cachen@gmail.com>
2021-11-26 02:39:39 +0100

    before zzz

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,64 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+
+const musicGenres = ["Blues", "Jazz", "Rap", "Reggae", "Rock"];
 
 const HomePage = () => {
-   
-        return (
-            <div className="homepage-container">
-                <label >Faites le choix d'un thème musical et on vous emmène</label>
-                <form className="homepage-form">
-                <div className="homepage-form-container">
-                <div className="homepage-form-toggle">
-                  <label htmlFor="Blues">Blues</label>
-                <input className="homepage-form-input"
-                type="checkbox"
-                name="Blues"
-                id="Blues"
-                />
-                </div>
-                <div className="homepage-form-toggle" >
-                <label htmlFor="Jazz">Jazz</label>
-                <input className="homepage-form-input"
-                type="checkbox"
-                name="Jazz"
-                id="Jazz"
-                />
-                </div>
-                <div className="homepage-form-toggle">
-                <label htmlFor="Rap">Rap</label>
-                <input className="homepage-form-input"
-                type="checkbox"
-                name="Rap"
-                id="Rap"
-                />
-                </div>
-                <div className="homepage-form-toggle">
-                <label htmlFor="Rock">Reggae</label>
-                <input className="homepage-form-input"
-                type="checkbox"
-                name="Reggae"
-                id="Reggae"
-                />
-                </div>
-                <div className="homepage-form-toggle">
-                <label htmlFor="Rock">Rock</label>
-                <input className="homepage-form-input" 
-                id="Rock"
-                type="checkbox"
-                name="Rock"
-                id="Rock"
-                />
-                </div>
-                </div>
-                <input className="homepage-form-input-budget"
-                type="text" 
-                id="budget"
-                placeholder="Votre budget"
-                />
-                <button className="homepage-form-button" type="submit">GO</button>
-              </form>
-            </div>
-          );
+  const [selectedGenre, setSelectedGenre] = useState("");
 
+  return (
+    <div className="homepage">
+      <div className="homepage-container">
+        <label>Faites le choix d'un thème musical et on vous emmène</label>
+        <form className="homepage-form">
+          <div className="homepage-form-container">
+            {musicGenres.map((genre) => (
+              <div className={`homepage-form-toggle ${selectedGenre === genre ? 'selected-chart' : ''}`}>
+                <label htmlFor={genre}>{genre}</label>
+                <input
+                  className="homepage-form-input"
+                  type="checkbox"
+                  name={genre}
+                  id={genre}
+                  value={genre}
+                  checked={selectedGenre === genre}
+                  onChange={(e) => setSelectedGenre(e.target.checked ? genre : '')}
+                />
+              </div>
+            ))}
+          </div>
+          <input
+            className="homepage-form-input-budget"
+            type="text"
+            id="budget"
+            placeholder="Votre budget"
+          />
+          <button className="homepage-form-button" type="submit">
+            GO
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
